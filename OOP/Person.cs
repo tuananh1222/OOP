ca0e1f940616b72788e0c58d0bb03d76755a933f
tuananh1222 <letuananh1222000@gmail.com>
2021-11-30 16:56:54 +0700

    Add project files.

---
+++ OOP/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_01
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public int Age { get; set; }
+        public decimal InCome { get; set; }
+        public float TaxCoe { get; set; }
+
+        public bool Equals(Person p)
+        {
+            if(!(Id == p.Id && Name == p.Name && Age == p.Age && InCome == p.InCome && TaxCoe == p.TaxCoe))
+            {
+                return false;
+            }
+            return true;
+        }
+        public decimal GetTax()
+        {
+            return InCome*(decimal)(1 - TaxCoe);
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,37 @@
+﻿using Bai_01;
+using System;
+using System.Collections.Generic;
+namespace Example1
+{
+    public class Program
+    {
+        static List<Person> person = null;
+        public static void Init()
+        {
+            person = new List<Person>();
+
+            person.Add(new Person { Id = 1, Name = "Le Tuan Anh", Age = 21, InCome = 2000, TaxCoe = 1 });
+            person.Add(new Person { Id = 2, Name = "Duong Tuan Anh", Age = 21, InCome = 2000, TaxCoe = 1 });
+            person.Add(new Person { Id = 3, Name = "Nguyen Tien Anh", Age = 21, InCome = 2000, TaxCoe = 1 });
+            person.Add(new Person { Id = 4, Name = "Nguyen Bao Trung", Age = 29, InCome = 10000, TaxCoe = 1 });
+            person.Add(new Person { Id = 5, Name = "Le Thi Linh", Age = 35, InCome = 2000, TaxCoe = 1 });
+            person.Add(new Person { Id = 6, Name = "Nguyen Thu Hoa", Age = 17, InCome = 1000, TaxCoe = 1 });
+            person.Add(new Person { Id = 7, Name = "Vu Tuan Anh", Age = 31, InCome = 13000, TaxCoe = 1 });
+            
+        }
+        public  static void Output()
+        {
+            foreach(var p in person)
+            {
+                Console.WriteLine($"Id: {p.Id} || Name: {p.Name} || Tax: {p.TaxCoe}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Init();
+            Output();
+
+            Console.ReadKey();
+        }
+    }
+}
